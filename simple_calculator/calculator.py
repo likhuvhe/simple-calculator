b827206f919f1b8f4ab3ccb78edb7f03255f50b5
likhuvhe <khuvhe.israel@gmail.com>
2021-02-18 21:47:58 +0200

    simple calculator

---
+++ simple_calculator/calculator.py
@@ -0,0 +1,11 @@
+def add(*num):
+    sum_num = 0
+    for n in num:
+        sum_num += n
+    return sum_num
+
+def multiply(*num):
+    mutiple = 1
+    for n in num:
+        mutiple *= n
+    return mutiple
---
+++ tests/test_calculator.py
@@ -0,0 +1,15 @@
+from simple_calculator.calculator import add, multiply
+
+def test_add():
+    assert add(1, 2) == 3
+def test_1_add():
+    assert add(-1, -1) == -2
+def test_2_add():
+    assert add(1,2,3,4,5) == 15
+
+def test_multiply():
+    assert multiply(1,3) == 3
+def test_1_multiply():
+    assert multiply(-1,3) == -3
+def test_2_multiply():
+    assert multiply(1,2,3,4,5) == 120
